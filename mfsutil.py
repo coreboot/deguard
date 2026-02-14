1583d3bcf3cb0dea474b3fe86e18071c43bd1e91
Ron Nazarov <ron@noisytoot.org>
2026-02-14 18:13:21 +0000

    Replace "#!/usr/bin/python3" with "#!/usr/bin/env python3"
    Makes this work on Guix, which has /usr/bin/env but does not install
    python3 into /usr/bin.
    
    Change-Id: I5da9d2e975f6465ce5eea27476152afffe301112
    Signed-off-by: Ron Nazarov <ron@noisytoot.org>
    Reviewed-on: https://review.coreboot.org/c/deguard/+/91223
    Tested-by: Máté Kukri <km@mkukri.xyz>
    Reviewed-by: Máté Kukri <km@mkukri.xyz>

--- mfsutil.py
+++ mfsutil.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 # SPDX-License-Identifier: GPL-2.0-only
 # This code is based on MFSUtil by Youness Alaoui (see `doc/LICENSE.orig` for original copyright)
 
